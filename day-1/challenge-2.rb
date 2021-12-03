32719cb49a6aa5f688531a4b5cc09ef06c2c0023
bateso88 <bateso@hotmail.co.uk>
2021-12-03 13:23:55 +0000

    Refactor

--- day-1/challenge-2.rb
+++ day-1/challenge-2.rb
@@ -1,5 +1,3 @@
 data = File.read("input.txt").split.map(&:to_i)
 
-sum_data = data.each_cons(3).map(&:sum)
-
-puts sum_data.each_cons(2) {|x, y| y > x }.count
+puts data.each_cons(3).map(&:sum).each_cons(2).select {|x| x[1] > x[0] }.count
