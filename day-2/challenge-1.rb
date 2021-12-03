f73531270bd5d9c96b7e67c0ed769af209643a9c
bateso88 <bateso@hotmail.co.uk>
2021-12-03 16:47:16 +0000

    Major refactor

--- day-2/challenge-1.rb
+++ day-2/challenge-1.rb
@@ -1,6 +1,6 @@
-data = File.read("input.txt").split.each_slice(2).to_a
+data = File.read("input.txt").split.each_slice(2).map { |move| { move[0].to_sym => move[1].to_i }}
 
-x = data.select { |move| move[0] == "forward" }.map{|move| move[1].to_i }.sum
-y = data.select { |move| move[0] != "forward" }.map { |move| move[0] == "up" ? -move[1].to_i : move[1].to_i }.sum
+h = Hash.new(0)
+sum = data.each_with_object(h) { |hash, h| hash.each { |key, value| h[key] += value }}
 
-p x, y, x*y
+p sum[:forward] * (sum[:down] - sum[:up])
