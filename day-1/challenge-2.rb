12dcd3dbaad09bac869d4d8d3524e6f2ef9de2f6
bateso88 <bateso@hotmail.co.uk>
2021-12-03 13:16:38 +0000

    Day 1 challenge 2

--- day-1/challenge-2.rb
+++ day-1/challenge-2.rb
@@ -2,4 +2,4 @@ data = File.read("input.txt").split.map(&:to_i)
 
 sum_data = data.each_cons(3).map(&:sum)
 
-puts sum_data.select.with_index {|depth, i| depth > sum_data[i-1] }.count
+puts sum_data.each_cons(2) {|x, y| y > x }.count
