ab1b90d856bf12d76be0ab965870535aa6f76e1b
bateso88 <bateso@hotmail.co.uk>
2021-12-03 16:21:37 +0000

    Refactor input data

--- day-2/challenge-1.rb
+++ day-2/challenge-1.rb
@@ -1,4 +1,4 @@
-data = File.read("input.txt").split("\n").map { |move| move.split }
+data = File.read("input.txt").split.each_slice(2).to_a
 
 x = data.select { |move| move[0] == "forward" }.map{|move| move[1].to_i }.sum
 y = data.select { |move| move[0] != "forward" }.map { |move| move[0] == "up" ? -move[1].to_i : move[1].to_i }.sum
