cfbbac800ebfec7c8cb430b235fde2dbef9f1cf7
bateso88 <bateso@hotmail.co.uk>
2021-12-03 15:54:53 +0000

    Refactor x coords

--- day-2/challenge-1.rb
+++ day-2/challenge-1.rb
@@ -3,6 +3,8 @@ data = File.read("input.txt").split("\n").map { |move| move.split }
 y = 0
 
 x = data.select {|move| move[0] == "forward"}.map{|move| move[1].to_i }.sum
+up = data.select {|move| move[0] == "up"}.map{|move| move[1].to_i }.sum
+down = data.select {|move| move[0] == "down"}.map{|move| move[1].to_i }.sum
 
 data.each do |move| 
   case move[0]
@@ -15,4 +17,5 @@ end
 
 p x
 p y
+p down - up 
 p x*y
