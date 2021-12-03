4bdc347a787f50cce3c26780cce3e0c6cc15f364
bateso88 <bateso@hotmail.co.uk>
2021-12-03 16:11:27 +0000

    Rerefactor y coords

--- day-2/challenge-1.rb
+++ day-2/challenge-1.rb
@@ -1,9 +1,6 @@
 data = File.read("input.txt").split("\n").map { |move| move.split }
 
-x = data.select {|move| move[0] == "forward"}.map{|move| move[1].to_i }.sum
-up = data.select {|move| move[0] == "up"}.map{|move| move[1].to_i }.sum
-down = data.select {|move| move[0] == "down"}.map{|move| move[1].to_i }.sum
+x = data.select { |move| move[0] == "forward" }.map{|move| move[1].to_i }.sum
+y = data.select { |move| move[0] != "forward" }.map { |move| move[0] == "up" ? -move[1].to_i : move[1].to_i }.sum
 
-p x
-p down - up 
-p x*(down - up)
+p x, y, x*y
