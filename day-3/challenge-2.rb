842353c8c3000d2df8c0b0e927b345ff98f3f164
bateso88 <bateso@hotmail.co.uk>
2021-12-10 15:27:56 +0000

    Refactor

--- day-3/challenge-2.rb
+++ day-3/challenge-2.rb
@@ -1,9 +1,9 @@
-def calculate(gas) # 0 for o2 and -1 for co2
-  data = File.read("input.txt").split.map(&:chars).map {|arr| arr.map(&:to_i) }
+def calculate(gas) # 0 for O2 and -1 for CO2
+  data = File.read("input.txt").split.map(&:chars).map {|bit| bit.map(&:to_i) }
   i=0
   until data.length == 1 
-    data.transpose[i].sum >= data.length.to_f/2 ? x = gas+1 : x = -gas
-    data.select! { |arr| arr[i] == x }
+    data.transpose[i].sum >= data.length.to_f/2 ? x = gas + 1 : x = -gas
+    data.select! { |bn| bn[i] == x }
     i+=1
   end
   data.join.to_i(2)
