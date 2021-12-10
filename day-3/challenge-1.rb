d0b7344d02d9eddee01c03dd073aaf6b452a1204
bateso88 <bateso@hotmail.co.uk>
2021-12-10 11:39:41 +0000

    Solve challenge 1 day 3

---
+++ day-3/challenge-1.rb
@@ -0,0 +1,6 @@
+data = File.read("input.txt").split.map(&:chars).map {|arr| arr.map(&:to_i) }
+
+gamma_rate = data.transpose.map(&:sum).map { |sum| sum > data.length/2 ? 1 : 0 }
+epsilon_rate = gamma_rate.map { |digit| digit == 1 ? 0 : 1 }
+
+p power_consumption = gamma_rate.join.to_i(2) * epsilon_rate.join.to_i(2)
