4e83a2eca1a80f994480c3988459fc373795c884
bateso88 <bateso@hotmail.co.uk>
2021-12-16 17:36:55 +0000

    Refactor

--- day-5/challenge-1.rb
+++ day-5/challenge-1.rb
@@ -1,38 +1,25 @@
 lines = File.read("input.txt")
             .split("\n")
             .map { |line| line.split(" -> ").map { |coords| coords.split(",").map(&:to_i) }}
-            # .select! { |line| x1(line) == x2(line) || y1(line) == y2(line) }
 
 vertical_lines = lines.select { |line| line[0][0] == line[1][0] }
 horizontal_lines = lines.select { |line| line[0][1] == line[1][1] }
 
-all_touched_coords = []
+all_coords = []
 
 vertical_lines.each do |line|
-  x_coord = line[0][0]
+  x = line[0][0]
   y = [line[0][1], line[1][1]].sort
-  (y[0]..y[1]).to_a.each { |y_coord| all_touched_coords << [x_coord, y_coord] }
+  (y[0]..y[1]).to_a.each { |y| all_coords << "#{[x,y]}" }
 end
 
 horizontal_lines.each do |line|
   x = [line[0][0], line[1][0]].sort
-  y_coord = line[0][1]
-  (x[0]..x[1]).to_a.each { |x_coord| all_touched_coords << [x_coord, y_coord] }
+  y = line[0][1]
+  (x[0]..x[1]).to_a.each { |x| all_coords << "#{[x,y]}" }
 end
-
-all_touched_coords.map! { |coords| "#{coords}" }
-
-p all_touched_coords.length
 
 sums = Hash.new(0)
+all_coords.each { |v| sums[v] += 1 }
 
-# iterate over the array, counting duplicate entries
-all_touched_coords.each do |v|
-  sums[v] += 1
-end
-
-p sums.length
-
-p sums.select { |k,v| v>1 }.length 
-
-
+p result = sums.select { |k,v| v>1 }.length 
