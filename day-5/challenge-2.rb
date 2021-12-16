71d88079d8db4d5c9c1a3355537302ecfe082b3c
bateso88 <bateso@hotmail.co.uk>
2021-12-16 19:16:19 +0000

    Refactor

--- day-5/challenge-2.rb
+++ day-5/challenge-2.rb
@@ -1,36 +1,33 @@
-lines = File.read("input.txt")
-            .split("\n")
-            .map { |line| line.split(" -> ").map { |coords| coords.split(",").map(&:to_i) }}
+class Challenge
 
-vertical_lines = lines.select { |line| line[0][0] == line[1][0] }
-horizontal_lines = lines.select { |line| line[0][1] == line[1][1] }
-diagonal_lines = lines.reject { |line| line[0][0] == line[1][0] || line[0][1] == line[1][1] }
+  attr_reader :lines, :all_coords, :sums
+  
+  def initialize
+    @lines = File.read("input.txt")
+                .split("\n")
+                .map { |line| line.split(" -> ").map { |coords| coords.split(",").map(&:to_i) }}
+    @all_coords = []
+    @sums = Hash.new(0)
+  end
 
-def ints_between(a,b)
-  a > b ? (b..a).to_a.reverse : (a..b).to_a
+  def result
+    lines.each { |line| add_coords(line)}
+    all_coords.each { |v| sums[v] += 1 }
+    sums.select { |k,v| v>1 }.length 
+  end
+
+  private
+
+  def ints_between(a,b)
+    a > b ? (b..a).to_a.reverse : (a..b).to_a
+  end
+
+  def add_coords(line)
+    x = ints_between(line[0][0], line[1][0])
+    y = ints_between(line[0][1], line[1][1])
+
+    y.each { |y| all_coords << "#{[x.sum,y]}" } if line[0][0] == line[1][0] #vertical
+    x.each { |x| all_coords << "#{[x,y.sum]}" } if line[0][1] == line[1][1] #horizontal
+    x.count.times { |i| all_coords << "#{[x[i],y[i]]}" } if line[0][0] != line[1][0] && line[0][1] != line[1][1] #diagonal
+  end
 end
-
-all_coords = []
-
-vertical_lines.each do |line|
-  x = line[0][0]
-  y = ints_between(line[0][1], line[1][1])
-  y.each { |y| all_coords << "#{[x,y]}" }
-end
-
-horizontal_lines.each do |line|
-  x = ints_between(line[0][0], line[1][0])
-  y = line[0][1]
-  x.each { |x| all_coords << "#{[x,y]}" }
-end
-
-diagonal_lines.each do |line|
-  x = ints_between(line[0][0], line[1][0])
-  y = ints_between(line[0][1], line[1][1])
-  x.count.times { |i| all_coords << "#{[x[i],y[i]]}" }
-end
-
-sums = Hash.new(0)
-all_coords.each { |v| sums[v] += 1 }
-
-p result = sums.select { |k,v| v>1 }.length 
