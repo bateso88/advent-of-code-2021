a8e3dda9714ec8c74c09b8b5a41e7943813841e1
bateso88 <bateso@hotmail.co.uk>
2021-12-17 15:04:05 +0000

    Refactor

--- day-8/challenge-1.rb
+++ day-8/challenge-1.rb
@@ -1,6 +1,5 @@
 codes = File.read("input.txt")
             .split("\n")
-            .map { |set| set.split(" | ").map{|data| data.split }}
-            .map { |set| set[1] }.flatten
+            .map { |set| set.split(" | ")[1].split }.flatten
 
 p codes.select { |code| code.length == 2 || code.length == 3 || code.length == 4 || code.length == 7 }.length
