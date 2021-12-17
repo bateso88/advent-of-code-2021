6c52d1ef12db8b1e11e8d56a3539582bf353a7e2
bateso88 <bateso@hotmail.co.uk>
2021-12-17 09:50:44 +0000

    Refactor

--- day-5/challenge-2.rb
+++ day-5/challenge-2.rb
@@ -1,19 +1,19 @@
 class Challenge
 
-  attr_reader :lines, :all_coords, :sums
+  attr_reader :lines, :touched_coords, :coord_frequencies
   
   def initialize
     @lines = File.read("input.txt")
-                .split("\n")
-                .map { |line| line.split(" -> ").map { |coords| coords.split(",").map(&:to_i) }}
-    @all_coords = []
-    @sums = Hash.new(0)
+                 .split("\n")
+                 .map { |line| line.split(" -> ").map { |coords| coords.split(",").map(&:to_i) }}
+    @touched_coords = []
+    @coord_frequencies = Hash.new(0)
   end
 
   def result
     lines.each { |line| add_coords(line)}
-    all_coords.each { |v| sums[v] += 1 }
-    sums.select { |k,v| v>1 }.length 
+    touched_coords.each { |v| coord_frequencies[v] += 1 }
+    @result ||= coord_frequencies.select { |k,v| v>1 }.length 
   end
 
   private
@@ -26,8 +26,20 @@ class Challenge
     x = ints_between(line[0][0], line[1][0])
     y = ints_between(line[0][1], line[1][1])
 
-    y.each { |y| all_coords << "#{[x.sum,y]}" } if line[0][0] == line[1][0] #vertical
-    x.each { |x| all_coords << "#{[x,y.sum]}" } if line[0][1] == line[1][1] #horizontal
-    x.count.times { |i| all_coords << "#{[x[i],y[i]]}" } if line[0][0] != line[1][0] && line[0][1] != line[1][1] #diagonal
+    y.each { |y| touched_coords << "#{[x.sum,y]}" } if vertical(line)
+    x.each { |x| touched_coords << "#{[x,y.sum]}" } if horizontal(line)
+    x.count.times { |i| touched_coords << "#{[x[i],y[i]]}" } if diagonal(line)
+  end
+
+  def vertical(line)
+    line[0][0] == line[1][0]
+  end
+
+  def horizontal(line)
+    line[0][1] == line[1][1]
+  end
+
+  def diagonal(line)
+    line[0][0] != line[1][0] && line[0][1] != line[1][1]
   end
 end
