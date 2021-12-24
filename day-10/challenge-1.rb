4840d9b31db0b6e2325752772133e50d4eca8e65
bateso88 <bateso@hotmail.co.uk>
2021-12-24 11:44:47 +0000

    Complete 10.1

---
+++ day-10/challenge-1.rb
@@ -0,0 +1,30 @@
+lines = File.read("input.txt").split.map { |row| row.chars }
+
+def is_opening?(bracket)
+  bracket == "(" || bracket == "[" || bracket == "{" || bracket == "<"
+end
+
+closing_bracket = { "(" => ")", "[" => "]", "{" => "}", "<" => ">" }
+bracket_score = { ")" => 3, "]" => 57, "}" => 1197, ">" => 25137 }
+errors = []
+score = 0
+
+lines.each.with_index do |line|
+  open_brackets = []
+  line.each.with_index do |bracket, index| 
+    if is_opening?(bracket)
+      open_brackets << bracket
+    else
+      if bracket != closing_bracket[open_brackets.pop] 
+        errors << bracket
+        break
+      end
+    end
+  end
+end
+
+errors.each { |bracket| score += bracket_score[bracket]}
+
+p errors
+
+p score
