266f46643eb87ffecc92bc5536302d55aed417d9
bateso88 <bateso@hotmail.co.uk>
2022-01-07 14:36:42 +0000

    Refactor

--- day-11/challenge-1.rb
+++ day-11/challenge-1.rb
@@ -1,15 +1,14 @@
 class Octopus 
 
-  attr_reader :y, :x, :neighbours, :energy, :has_flashed, :needs_to_update_neighbours
+  attr_reader :y, :x, :neighbours, :energy, :has_flashed, :need_to_update_neighbours
 
   def initialize(y, x, energy)
     @y = y
     @x = x
     @energy = energy.to_i
-    @needs_to_update_neighbours = false
-    @neighbours = []
-    find_neighbours
+    @need_to_update_neighbours = false
     @has_flashed = false
+    find_neighbours
   end
 
   def increment_energy
@@ -17,13 +16,13 @@ class Octopus
     @energy += 1
     if energy > 9
       @has_flashed = true
-      @needs_to_update_neighbours = true
+      @need_to_update_neighbours = true
       @energy = 0
     end
   end
 
   def reset_need_to_update_neighbours
-    @needs_to_update_neighbours = false
+    @need_to_update_neighbours = false
   end
 
   def reset_flash
@@ -33,6 +32,7 @@ class Octopus
   private 
 
   def find_neighbours
+    @neighbours = []
     #above
     neighbours << [y-1,x-1] if x > 0 && y > 0
     neighbours << [y-1,x] if y > 0
@@ -48,7 +48,7 @@ class Octopus
 end
 
 
-class Game
+class Challenge
 
   attr_reader :octopuses, :flash_count
   
@@ -57,25 +57,20 @@ class Game
     @coords_to_update = []
     @flash_count = 0
   end
-  
-  def step
-    # Increases each octopus' energy by 1. If octopus flashes, sets flash to true, sets needs_to_update_neighbours to true and energy to 0  
-    pod.each { |octopus| octopus.increment_energy }
 
-    @coords_to_update = [1]
-    until @coords_to_update.length == 0
-      update_neighbours
-    end
-
-    @flash_count += pod.select {|octopus| octopus.has_flashed }.count
-
-    pod.each { |octopus| octopus.reset_flash }
-
-    show
-    p flash_count
+  def result
+    100.times { step }
+    flash_count
   end
   
   private
+  
+  def step
+    increment_energy(pod)
+    update_neighbours
+    update_flash_count
+    show
+  end
 
   def show 
     octopuses.each { |row| p row.map { |octopus| octopus.energy }}
@@ -88,7 +83,7 @@ class Game
   end
 
   def coords_to_update
-    @coords_to_update.length == 0 ? @coords_to_update : @coords_to_update = @coords_to_update.flatten.each_slice(2).map {|coords| [coords[0], coords[1]]}
+    @coords_to_update = @coords_to_update.flatten.each_slice(2).map {|coords| [coords[0], coords[1]]}
   end
 
   def octopuses_to_update
@@ -100,16 +95,21 @@ class Game
   end
 
   def update_neighbours
-    # p @coords_to_update
-    @coords_to_update = []
-    # Records the coords of every neighbour of a flashing octopus
-    pod.each { |octopus| @coords_to_update << octopus.neighbours if octopus.needs_to_update_neighbours }
-    # Now that the coords are stored, we can reset all of the octopuses' needs_to_update_neighbours to false
-    pod.each { |octopus| octopus.reset_need_to_update_neighbours }
+    loop do 
+      @coords_to_update = []
+      pod.each { |octopus| @coords_to_update << octopus.neighbours if octopus.need_to_update_neighbours }
+      pod.each { |octopus| octopus.reset_need_to_update_neighbours }
+      increment_energy(octopuses_to_update)
+      break if @coords_to_update.length == 0
+    end
+  end
 
-    # show
+  def update_flash_count
+    @flash_count += pod.select {|octopus| octopus.has_flashed }.count
+    pod.each { |octopus| octopus.reset_flash }
+  end
 
-    # Increment the neighbouring octopuses of a flash
-    octopuses_to_update.each { |octopus| octopus.increment_energy }
+  def increment_energy(octopi)
+    octopi.each { |octopus| octopus.increment_energy }
   end
 end
