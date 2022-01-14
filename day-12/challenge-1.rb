2a58914404da05aa5d746cc9dcc524c6cf9c890e
bateso88 <bateso@hotmail.co.uk>
2022-01-14 12:10:53 +0000

    Complete 12.1

--- day-12/challenge-1.rb
+++ day-12/challenge-1.rb
@@ -10,8 +10,6 @@ attr_reader :neighbours, :name, :category
   end
 
   def category
-    return "START" if name == "start"
-    return "END" if name == "end"
     return "SMALL" if name == name.downcase
     return "BIG" if name == name.upcase
   end
@@ -19,27 +17,55 @@ end
 
 class Challenge
 
-  attr_reader :lines, :points, :paths, :start_point
-
   def initialize
     @lines = File.read("input.txt").split.map { |line| line.split("-") }
     @points = lines.flatten.uniq.map{ |name| Point.new(name, lines)}
-    @paths = []
-    @start_point = points.find{ |point| point.category == "START" }
-    @end_point = points.find{ |point| point.category == "END" }
+    @paths = [[find_point("start")]]
   end
 
-  def create_path
-    paths << start_point
+  def result
+    until paths.all? { |path| path[-1].name == "end"}
+      iterate_paths
+    end
+    show_paths
+    p paths.length
+  end
 
-    #### NEEDS A LOT OF WORK 
-    #Something like this but obvs need to be iterative
-    new_paths = [
-      [paths[-1], find_point(paths[-1].neighbours[0])], 
-      [paths[-1], find_point(paths[-1].neighbours[1])], 
-      [paths[-1], find_point(start_point.neighbours[2])],
-    ]
-    
+  private
+
+  attr_reader :lines, :points, :paths
+
+  def iterate_paths
+    new_paths = []
+
+    paths.each do |path|
+      # skip (and keep) path if its reached the end
+      if path[-1].name == "end"
+        new_paths << path
+        next
+      end
+
+      path[-1].neighbours.each do |neighbour|
+        new_point = find_point(neighbour)
+
+        # skip point if it is already in the path AND it is small
+        next if path.include?(new_point) && new_point.category == "SMALL"
+        
+        new_paths << extended_path(path, new_point)
+      end
+    end
+
+    @paths = new_paths
+  end
+
+  def extended_path(path, point)
+    new_path = path.clone
+    new_path << point
+  end
+
+  def show_paths
+    paths.each { |path| p path.map { |point| point.name }}
+    return
   end
 
   def find_point(name)
