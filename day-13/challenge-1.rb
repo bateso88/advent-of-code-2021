f4cb9723fbaada76e2e141c3a7534fc6b1167f21
bateso88 <bateso@hotmail.co.uk>
2022-01-14 15:00:04 +0000

    Set up 13

--- day-13/challenge-1.rb
+++ day-13/challenge-1.rb
@@ -1,12 +1,4 @@
 # fold along x=655
-# fold along y=447
-# fold along x=327
-# fold along y=223
-# fold along x=163
-# fold along y=111
-# fold along x=81
-# fold along y=55
-# fold along x=40
-# fold along y=27
-# fold along y=13
-# fold along y=6
+lines = File.read("input.txt").split.map { |line| line.split(",").map(&:to_i) }
+
+p lines
