325236b90bfb55c6cdd38e9055e505102c8d7175
bateso88 <bateso@hotmail.co.uk>
2022-01-14 16:34:11 +0000

    Refactor

--- day-13/challenge-2.rb
+++ day-13/challenge-2.rb
@@ -7,9 +7,8 @@ class Challenge
   end
 
   def result
-    do_all_folds
-    x_max = coords.transpose[0].max + 1 
-    y_max = coords.transpose[1].max + 1
+    all_folds
+
     grid = Array.new(y_max)
     y_max.times do |row_index|
       grid[row_index] = Array.new(x_max)
@@ -22,33 +21,34 @@ class Challenge
 
   private
 
-  def do_all_folds # as per instructions
-    fold("x", 655)
-    fold("y", 447)
-    fold("x", 327)
-    fold("y", 223)
-    fold("x", 163)
-    fold("y", 111)
-    fold("x", 81)
-    fold("y", 55)
-    fold("x", 40)
-    fold("y", 27)
-    fold("y", 13)
-    fold("y", 6)
+  def x_max
+    coords.transpose[0].max + 1
   end
 
-  def fold(axis, value)
-    fold_x_axis(value) if axis == "x"
-    fold_y_axis(value) if axis == "y"
+  def y_max
+    coords.transpose[1].max + 1
   end
 
-  def fold_x_axis(value)
+  def all_folds # as per instructions
+    fold_x(655)
+    fold_y(447)
+    fold_x(327)
+    fold_y(223)
+    fold_x(163)
+    fold_y(111)
+    fold_x(81)
+    fold_y(55)
+    fold_x(40)
+    fold_y(27)
+    fold_y(13)
+    fold_y(6)
+  end
+
+  def fold_x(value)
     @coords.map! { |x,y| x > value ? [(2 * value) - x, y] : [x, y] }.uniq!
   end
 
-  def fold_y_axis(value)
+  def fold_y(value)
     @coords.map! { |x,y| y > value ? [x, (2 * value) - y] : [x, y] }.uniq!
   end
 end
-
-
