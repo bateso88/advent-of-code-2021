ffcec8685b22d5b181e18165a1f32bf202ce5a60
bateso88 <bateso@hotmail.co.uk>
2022-01-28 10:57:20 +0000

    Complete 14.1

---
+++ day-14/challenge-1.rb
@@ -0,0 +1,23 @@
+class Challenge
+
+  attr_reader :polymer, :codes
+
+  def initialize
+    @polymer = "NBOKHVHOSVKSSBSVVBCS"
+    @codes = File.read("input.txt").split("\n").map { |line| line.split(" -> ") }.map { |line| [line[0].chars, line[1]] }
+  end
+
+  def result 
+    10.times { step }
+    polymer
+  end
+
+  def step
+    @polymer = polymer.chars.each_cons(2).map { |pair| codes.find { |code| code[0] == pair }}
+    polymer.map! { |code| [code[0][0], code[1], code[0][1]] }
+    first_item = polymer.shift
+    polymer.each{ |item| item.shift }
+    @polymer = (first_item + polymer).flatten.join
+  end
+end
+
